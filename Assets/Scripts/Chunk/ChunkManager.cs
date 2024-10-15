3fc7c517232155e732dc5a9138d9a5906a0dfbd5
octopusVick <wangsongquan9@gmail.com>
2024-10-15 14:46:07 +0800

    10.15.3
    chunkManager优化

--- Assets/Scripts/Chunk/ChunkManager.cs
+++ Assets/Scripts/Chunk/ChunkManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.Pool; // 添加这行，如果你使用的是 Unity 2020.1 或更新版本
 
 public class ChunkManager : MonoBehaviour
 {
@@ -14,6 +15,14 @@ public class ChunkManager : MonoBehaviour
     private Dictionary<Vector2Int, HashSet<GameObject>> chunkObjects = new Dictionary<Vector2Int, HashSet<GameObject>>();
     private HashSet<Vector2Int> visibleChunks = new HashSet<Vector2Int>();
     private Vector2Int currentChunk;
+
+    // 如果使用 Unity 2020.1 或更新版本，使用内置的 ObjectPool
+    private ObjectPool<HashSet<GameObject>> chunkObjectSetPool;
+
+    // 如果使用较旧版本的 Unity，使用自定义的 SimpleObjectPool
+    // private SimpleObjectPool<HashSet<GameObject>> chunkObjectSetPool;
+
+    private Dictionary<Vector2Int, BoundsInt> chunkBoundsCache = new Dictionary<Vector2Int, BoundsInt>();
 
     private void Start()
     {
@@ -42,34 +51,35 @@ public class ChunkManager : MonoBehaviour
 
     private void UpdateVisibleChunks()
     {
-        HashSet<Vector2Int> newVisibleChunks = new HashSet<Vector2Int>();
+        // 使用 HashSet<Vector2Int>.Enumerator 来避免额外的内存分配
+        HashSet<Vector2Int>.Enumerator enumerator = visibleChunks.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            Vector2Int chunk = enumerator.Current;
+            if (!IsChunkVisible(chunk))
+            {
+                SetChunkVisibility(chunk, false);
+            }
+        }
 
         for (int x = -loadDistance; x <= loadDistance; x++)
         {
             for (int y = -loadDistance; y <= loadDistance; y++)
             {
                 Vector2Int coord = new Vector2Int(currentChunk.x + x, currentChunk.y + y);
-                newVisibleChunks.Add(coord);
+                if (!visibleChunks.Contains(coord))
+                {
+                    SetChunkVisibility(coord, true);
+                    visibleChunks.Add(coord);
+                }
             }
         }
+    }
 
-        foreach (Vector2Int chunk in visibleChunks)
-        {
-            if (!newVisibleChunks.Contains(chunk))
-            {
-                SetChunkVisibility(chunk, false);
-            }
-        }
-
-        foreach (Vector2Int chunk in newVisibleChunks)
-        {
-            if (!visibleChunks.Contains(chunk))
-            {
-                SetChunkVisibility(chunk, true);
-            }
-        }
-
-        visibleChunks = newVisibleChunks;
+    private bool IsChunkVisible(Vector2Int chunk)
+    {
+        return Mathf.Abs(chunk.x - currentChunk.x) <= loadDistance &&
+               Mathf.Abs(chunk.y - currentChunk.y) <= loadDistance;
     }
 
     private void SetChunkVisibility(Vector2Int chunkCoord, bool visible)
@@ -83,30 +93,25 @@ public class ChunkManager : MonoBehaviour
         {
             foreach (GameObject obj in objects)
             {
-                if (obj != null)
-                {
-                    if (visible)
-                    {
-                        EnableObject(obj);
-                    }
-                    else
-                    {
-                        DisableObject(obj);
-                    }
-                }
+                SetObjectVisibility(obj, visible);
             }
         }
     }
 
     private void SetTilemapChunkVisibility(Tilemap tilemap, Vector2Int chunkCoord, bool visible)
     {
-        Vector3Int chunkOrigin = new Vector3Int(
-            chunkCoord.x * chunkWidth - chunkWidth / 2,
-            chunkCoord.y * chunkHeight - chunkHeight / 2,
-            0
-        );
-        BoundsInt bounds = new BoundsInt(chunkOrigin, new Vector3Int(chunkWidth, chunkHeight, 1));
+        if (!chunkBoundsCache.TryGetValue(chunkCoord, out BoundsInt bounds))
+        {
+            Vector3Int chunkOrigin = new Vector3Int(
+                chunkCoord.x * chunkWidth - chunkWidth / 2,
+                chunkCoord.y * chunkHeight - chunkHeight / 2,
+                0
+            );
+            bounds = new BoundsInt(chunkOrigin, new Vector3Int(chunkWidth, chunkHeight, 1));
+            chunkBoundsCache[chunkCoord] = bounds;
+        }
 
+        // 使用缓存的 bounds
         foreach (Vector3Int pos in bounds.allPositionsWithin)
         {
             tilemap.SetTileFlags(pos, TileFlags.None);
@@ -134,11 +139,12 @@ public class ChunkManager : MonoBehaviour
 
     private void AddObjectToChunk(Vector2Int chunkCoord, GameObject obj)
     {
-        if (!chunkObjects.ContainsKey(chunkCoord))
+        if (!chunkObjects.TryGetValue(chunkCoord, out HashSet<GameObject> objects))
         {
-            chunkObjects[chunkCoord] = new HashSet<GameObject>();
+            objects = chunkObjectSetPool.Get();
+            chunkObjects[chunkCoord] = objects;
         }
-        chunkObjects[chunkCoord].Add(obj);
+        objects.Add(obj);
     }
 
     public void ForceUpdateChunks(Vector3 cameraPosition)
@@ -168,4 +174,70 @@ public class ChunkManager : MonoBehaviour
         }
         obj.SetActive(false);
     }
+
+    // 添加这个方法来处理单个对象的可见性
+    private void SetObjectVisibility(GameObject obj, bool visible)
+    {
+        if (obj != null)
+        {
+            if (visible)
+            {
+                EnableObject(obj);
+            }
+            else
+            {
+                DisableObject(obj);
+            }
+        }
+    }
+
+    // 添加这个方法来清理不再使用的 chunk
+    private void CleanupChunk(Vector2Int chunkCoord)
+    {
+        if (chunkObjects.TryGetValue(chunkCoord, out HashSet<GameObject> objects))
+        {
+            chunkObjectSetPool.Release(objects);
+            chunkObjects.Remove(chunkCoord);
+        }
+    }
+
+    private void Awake()
+    {
+        // 如果使用 Unity 2020.1 或更新版本，初始化 ObjectPool
+        chunkObjectSetPool = new ObjectPool<HashSet<GameObject>>(
+            createFunc: () => new HashSet<GameObject>(),
+            actionOnRelease: set => set.Clear()
+        );
+
+        // 如果使用较旧版本的 Unity，初始化 SimpleObjectPool
+        // chunkObjectSetPool = new SimpleObjectPool<HashSet<GameObject>>(
+        //     () => new HashSet<GameObject>(),
+        //     set => set.Clear()
+        // );
+    }
 }
+
+// 如果使用较旧版本的 Unity，添加这个简单的对象池实现
+// public class SimpleObjectPool<T>
+// {
+//     private readonly System.Func<T> createFunc;
+//     private readonly System.Action<T> actionOnRelease;
+//     private readonly Stack<T> pool = new Stack<T>();
+// 
+//     public SimpleObjectPool(System.Func<T> createFunc, System.Action<T> actionOnRelease)
+//     {
+//         this.createFunc = createFunc;
+//         this.actionOnRelease = actionOnRelease;
+//     }
+// 
+//     public T Get()
+//     {
+//         return pool.Count > 0 ? pool.Pop() : createFunc();
+//     }
+// 
+//     public void Release(T item)
+//     {
+//         actionOnRelease(item);
+//         pool.Push(item);
+//     }
+// }
